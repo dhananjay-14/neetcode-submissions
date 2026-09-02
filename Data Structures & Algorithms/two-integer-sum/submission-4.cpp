6f291595fcd53176dba5789bf94e6cbbfc61b610
Dhananjay Bhavar <101885788+dhananjay-14@users.noreply.github.com>
2026-09-02 23:41:51 +0530

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+        vector<int> result;
+        for(int i = 0;i<nums.size();i++){
+            int complement = target - nums[i];
+            if(mp.find(complement)!=mp.end()){
+                result.push_back(mp[complement]);
+                result.push_back(i);
+                break;
+            }
+            mp[nums[i]] = i;
+        }
+        return result;
+    }
+};
