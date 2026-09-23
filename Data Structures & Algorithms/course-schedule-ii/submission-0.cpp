34af106c4a11226346c070d6bc513242e16d65f9
dhananjay.bhavar@pw.live <101885788+dhananjay-14@users.noreply.github.com>
2026-09-23 22:51:01 +0530

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool solve(vector<vector<int>>& graph,
+               vector<int>& visited,
+               vector<int>& pathVisited,
+               int num, stack<int>&st) {
+
+        visited[num] = 1;
+        pathVisited[num] = 1;
+
+        for (auto el : graph[num]) {
+
+            if (!visited[el]) {
+                if (solve(graph, visited, pathVisited, el,st))
+                    return true;
+            }
+            else if (pathVisited[el]) {
+                return true;  // cycle
+            }
+        }
+        st.push(num);
+        pathVisited[num] = 0;
+        return false;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+         vector<vector<int>> graph(numCourses);
+         vector<int>res;
+         stack<int>st;
+
+        for (auto el : prerequisites) {
+            int nxt = el[0];
+            int fst = el[1];
+
+            graph[fst].push_back(nxt);
+        }
+
+        vector<int> visited(numCourses, 0);
+        vector<int> pathVisited(numCourses, 0);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!visited[i]) {
+                if (solve(graph, visited, pathVisited, i,st))
+                    return res;
+            }
+        }
+
+        while(!st.empty()){
+            int el = st.top();
+            st.pop();
+            res.push_back(el);
+        }
+        return res;
+        }
+
+};
